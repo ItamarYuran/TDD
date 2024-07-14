e14d4e123162fa3ab041f84ff7dc5fdd93877c68
Itamar Yuran <itamaryuran@gmail.com>
2024-07-14 19:00:36 +0300

    3

---
+++ Hello world/hello.go
@@ -0,0 +1,17 @@
+package main
+
+const englishHelloPrefix = "Hello, "
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	if language == "Spanish" {
+		return "Hola, " + name
+
+	}
+	return englishHelloPrefix + name
+}
+func main() {
+	//fmt.Println(Hello("Itamar"))
+}
